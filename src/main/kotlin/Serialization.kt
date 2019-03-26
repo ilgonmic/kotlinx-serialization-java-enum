797af78fe5be026ecf2cc5576e50575711057601
ilgonmic <ilgonmic@gmail.com>
2019-03-26 22:40:45 +0300

    Add java enum serialization

--- src/main/kotlin/Serialization.kt
+++ src/main/kotlin/Serialization.kt
@@ -1,4 +1,24 @@
-import kotlinx.serialization.Serializable
+@file:UseSerializers(
+    TelephoneSerializer::class
+)
+
+import kotlinx.serialization.*
+import kotlinx.serialization.internal.StringDescriptor
 
 @Serializable
 data class Data(val telephone: Telephone)
+
+private object TelephoneSerializer : KSerializer<Telephone> {
+    override val descriptor: SerialDescriptor
+        get() = StringDescriptor.withName("Telephone")
+
+    override fun deserialize(decoder: Decoder): Telephone {
+        val decoded = decoder.decodeString().toUpperCase()
+        return Telephone.valueOf(decoded)
+    }
+
+    override fun serialize(encoder: Encoder, obj: Telephone) {
+        TODO()
+    }
+
+}
